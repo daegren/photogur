2f62d88a82184a3aaddf3b855044266bfcaf6e91
Monica Olinescu <monica.olinescu@gmail.com>
2013-02-11 12:24:52 -0500

    Lab1 solution - Loop through the pictures in the array and display them in pictures#index

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,10 @@
 class PicturesController < ApplicationController
 
   def index
+    @pictures = [
+      'http://monicao.s3.amazonaws.com/bitmaker/house.jpg', 
+      'http://monicao.s3.amazonaws.com/bitmaker/wave.jpg', 
+      'http://monicao.s3.amazonaws.com/bitmaker/girl.jpg'
+    ]
   end
 end
