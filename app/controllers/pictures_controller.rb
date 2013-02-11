b5cf1c88d2a6d0692b60740e1f225a1f692169a5
Monica Olinescu <monica.olinescu@gmail.com>
2013-02-11 12:42:54 -0500

    Lab2 setup: Store the picture information in a hash instead of an array

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,9 +2,21 @@ class PicturesController < ApplicationController
 
   def index
     @pictures = [
-      'http://monicao.s3.amazonaws.com/bitmaker/house.jpg', 
-      'http://monicao.s3.amazonaws.com/bitmaker/wave.jpg', 
-      'http://monicao.s3.amazonaws.com/bitmaker/girl.jpg'
+      {
+        :title  => "The old church on the coast of White sea",
+        :artist => "Sergey Ershov",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+      },
+      {
+        :title  => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+      },
+      {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+      }
     ]
   end
 end
